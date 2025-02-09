c4b361847f17d98066c84ea76e0f1f8839a50028
Lynx <148461348+LynxLenior@users.noreply.github.com>
2025-02-09 10:20:55 +0000

    Scripting

--- someJava.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".hover-btn").addEventListener("mouseenter", function() {
-    document.querySelector(".pictures").style.backgroundImage = "url('https://raw.githubusercontent.com/LynxLenior/Froggy/main/Image/Yipee.gif')";
-});
-
-document.querySelector(".hover-btn").addEventListener("mouseleave", function() {
-    document.querySelector(".pictures").style.backgroundImage = "url('https://raw.githubusercontent.com/LynxLenior/Froggy/main/Image/CatRose.jpg')";
-});
